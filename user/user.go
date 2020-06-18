2b5cb0a77996deab92c3545c038d8de6e052e01e
Cagox Media <45616578+cagox@users.noreply.github.com>
2020-06-18 14:51:21 -0700

    type  CreateUserForm struct
    I have added the CreateUserForm struct to facilitate passing the information to the CreateUserFromForm function.
    At this time, all that is needed is the email address and the password.  Initially, form validation will be driven by the
    handler function, so we do  not need to pass the admin token or password2 to the function that creates the user.

--- user/user.go
+++ user/user.go
@@ -9,12 +9,19 @@ import (
 
 //User is meant to hold user related information in the Database.
 type User struct {
-	ID            bson.ObjectId `bson:"_id,omitempty"`
-	Email         string
-	PasswordHash  string
-  Timestamp     time.Time
-  LastUpdated   time.Time
+    ID            bson.ObjectId `bson:"_id,omitempty"`
+    Email         string
+    PasswordHash  string
+    Timestamp     time.Time
+    LastUpdated   time.Time
 }
+
+//CreateUserForm is a struct to facilitate creating user objects.
+type CreateUserForm struct {
+    Email     string
+    Password  string
+}
+
 
 func init() {
 	gob.Register(User{})
